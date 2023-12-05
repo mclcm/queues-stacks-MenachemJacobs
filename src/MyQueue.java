67e26548a08fb60a4d586c253df2d18a70f248ad
Menolo-Homobovanez <118155256+Menolo-Homobovanez@users.noreply.github.com>
2023-12-05 15:43:25 -0500

    javaDocs

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -9,7 +9,6 @@ public class MyQueue{
     private int size = 0;
     private int front;
     private int back;
-    boolean overFlowFlag;
 
     public MyQueue() {
         this(DEFAULT_INITIAL_CAPACITY);
@@ -23,20 +22,36 @@ public class MyQueue{
         backingStore = new String[capacity];
     }
 
-    int size() {
+    /**
+     * Returns the current size of the queue.
+     *
+     * @return The number of elements in the queue.
+     */
+    public int size() {
         return size;
     }
 
-    boolean isEmpty() {
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return true if the queue is empty, false otherwise.
+     */
+    public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Enqueues a new element at the back of the queue.
+     *
+     * @param s The string to be enqueued.
+     * @throws IllegalArgumentException if the queue is full.
+     */
     public void enqueue(String s){
         //Todo find out how this should actually be handled
         if(size + 1 == Integer.MAX_VALUE)
             throw new IllegalArgumentException("may not add more values to this Stack");
 
-        if(size() == backingStore.length)
+        if(size == backingStore.length)
             refactor();
 
         back = (back + 1) % backingStore.length;
@@ -45,6 +60,12 @@ public class MyQueue{
         backingStore[back] = s;
     }
 
+    /**
+     * Dequeues an element from the front of the queue.
+     *
+     * @return The dequeued string.
+     * @throws IllegalStateException if the queue is empty.
+     */
     public String dequeue(){
         String returnVal = backingStore[front];
         front = (front + 1) % backingStore.length;
@@ -53,7 +74,12 @@ public class MyQueue{
         return returnVal;
     }
 
-    String[] toArray(){
+    /**
+     * Returns an array representation of the elements in the queue.
+     *
+     * @return An array of strings representing the elements in the queue.
+     */
+    public String[] toArray(){
         String[] outray = new String[size];
         for (int i = 0; i < backingStore.length; i++) {
             outray[i] = backingStore[(i + front) % backingStore.length];
@@ -61,7 +87,11 @@ public class MyQueue{
         return outray;
     }
 
-    void refactor(){
+    /**
+     * Refactors the backing store array to accommodate more elements.
+     * Invoked when the current capacity is reached during an enqueue operation.
+     */
+    private void refactor(){
         String[] holder = backingStore;
         backingStore = new String[backingStore.length + DEFAULT_INITIAL_CAPACITY];
         for (int i = 0; i < backingStore.length; i++) {
