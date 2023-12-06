169c3f9b72cc7ac96aa4ef2862752c49682efbf9
Menolo-Homobovanez <118155256+Menolo-Homobovanez@users.noreply.github.com>
2023-12-05 19:13:33 -0500

    BracketCheck class
    two arrays track all bracket types for which the validator is good.
    Uses a stack to track the most recent seen opening type bracket. If closing types are seen, they are checked against the top of the stack.
    If a match is found, the stack is popped.
    If a match is not found, or the stack is empty, validator returns false

--- src/BracketCheck.java
+++ src/BracketCheck.java
@@ -1,2 +1,56 @@
+import java.util.Stack;
+
 public class BracketCheck {
+    private Stack<Character> bracketStack;
+    private char[] openers = new char[]{'(', '[', '{'};
+    private char[] closers = new char[]{')', ']', '}'};
+
+    public boolean isValid(String s) {
+        bracketStack = new Stack<>();
+
+        //look through every char of the string
+        for (char c : s.toCharArray()) {
+            //if it's an opener add it to the stack
+            if (isOpener(c))
+                bracketStack.push(c);
+
+            if (isCloser(c)) {
+                //if it's a closer, but the stack is empty, or it doesn't match the most recent opener, return false
+                if (bracketStack.isEmpty() || !isBracketMatch(bracketStack.peek(), c))
+                    return false;
+                //if it's a closer and it matches the most recent opener, pop the stack
+                else if (isBracketMatch(bracketStack.peek(), c))
+                    bracketStack.pop();
+            }
+        }
+
+        return bracketStack.isEmpty();
+    }
+
+    private boolean isOpener(char c) {
+        for (char letter : openers) {
+            if (c == letter)
+                return true;
+        }
+
+        return false;
+    }
+
+    private boolean isCloser(char c) {
+        for (char letter : closers) {
+            if (c == letter)
+                return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBracketMatch(char rBracket, char lBracket) {
+        return switch (rBracket) {
+            case '(' -> lBracket == ')';
+            case '[' -> lBracket == ']';
+            case '{' -> lBracket == '}';
+            default -> false;
+        };
+    }
 }
