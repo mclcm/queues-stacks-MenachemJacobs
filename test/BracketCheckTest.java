508fbf07c2790b60d1f3e567b70a32d5c0808e1f
Menolo-Homobovanez <118155256+Menolo-Homobovanez@users.noreply.github.com>
2023-12-06 19:50:44 -0500

    isValid simple tests

--- test/BracketCheckTest.java
+++ test/BracketCheckTest.java
@@ -1,5 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BracketCheckTest {
+    BracketCheck bracketTester;
 
+    @BeforeEach
+    void setUp() {
+        bracketTester = new BracketCheck();
+    }
+
+    @Test
+    void bracketCheck_Normal(){
+        assertTrue(bracketTester.isValid("()"), "Simple case returned false when it should not have");
+        assertFalse(bracketTester.isValid("(}"), "Simple case returned true when it should not have");
+    }
 }
